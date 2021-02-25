87463b1afe40a1e8b99e633d9de31389f30b8407
GregorDevelop <kramere26@gmail.com>
2021-02-25 07:34:17 +0100

    Fourth commit  - Table view is done, cells is created

--- The Quiz App/Controller/ViewController.swift
+++ The Quiz App/Controller/ViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, QuizProtocol {
+class ViewController: UIViewController, QuizProtocol, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var model = QuizModel()
     var questions = [Question]()
@@ -20,15 +23,47 @@ class ViewController: UIViewController, QuizProtocol {
         model.delegate = self
         model.getQuestions()
         
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-    //MARK: - QuestionsRetrieved protocol 
-    
+    // MARK: - QuizProtocol Methods
+
     func questionsRetrieved(_ questions: [Question]) {
-
+        // Get a reference to the questions
         self.questions = questions
-        print(questions)
     }
 
+    // MARK: - UITableView DataSource and Delegate Methods
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Make sure that the questions array actually contains at least a question
+        guard questions.count > 0 else {return 0}
+        
+        // Return the number of answers for this question
+        let currentQuestion = questions[currentQuestionIndex]
+        
+        if currentQuestion.answers != nil {
+            return currentQuestion.answers!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let label = cell.viewWithTag(1) as? UILabel
+        
+        if label != nil {
+            
+            
+        }
+        
+        return cell
+    }
+    
 }
 
